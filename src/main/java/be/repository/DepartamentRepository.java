1c9ab80777a3f70b714c63f8cdc30445c2122f6c
ionutiovaa <52444181+ionutiovaa@users.noreply.github.com>
2022-11-14 20:45:32 +0200

    Delete src/main/java/be/repository directory
    Delete unused files

--- src/main/java/be/repository/DepartamentRepository.java
+++
@@ -1,9 +0,0 @@
-package be.repository;
-
-import be.entity.Departament;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartamentRepository extends JpaRepository<Departament, Integer> {
-}
--- src/main/java/be/repository/ProiectRepository.java
+++
@@ -1,9 +0,0 @@
-package be.repository;
-
-import be.entity.Proiect;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProiectRepository extends JpaRepository<Proiect, Integer> {
-}
